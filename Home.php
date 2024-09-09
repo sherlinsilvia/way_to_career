e6d0e4a967a0458843c19a6ecae5f0c5e773be67
sherlinsilvia <155231823+sherlinsilvia@users.noreply.github.com>
2024-09-09 21:23:09 +0530

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Document</title>
+</head>
+<style>
+  body{
+    background-image: url('bg3.gif');
+    background-size: cover;
+    
+    
+    
+    height: 100vh;
+    padding:0;
+    margin:0;
+}
+   ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color:grey;
+}
+footer {
+  text-align: center;
+  padding: 3px;
+  background-color:grey;
+  color: white;
+}
+
+</style>
+<body>
+    <img src="bg13.jpg.png" class="img-circle" alt="logo image" width="200" height="136"> 
+    <ul>
+      <li><a href="home.php">Home</a></li>
+      <li><a href="signin.php">sign Up</a></li>
+      <li><a href="login.php">Login</a></li>
+    </ul>
+    
+    <br><br>
+    <center>
+        <h1>"𝕰𝖉𝖚𝖈𝖆𝖙𝖎𝖔𝖓 𝖎𝖘 𝖙𝖍𝖊 𝖒𝖔𝖘𝖙  𝖕𝖔𝖜𝖊𝖗𝖋𝖚𝖑𝖑  𝖜𝖊𝖆𝖕𝖔𝖓 𝖜𝖍𝖎𝖈𝖍 𝖞𝖔𝖚 𝖈𝖆𝖓 𝖚𝖘𝖊 𝖙𝖔 𝖈𝖍𝖆𝖓𝖌𝖊 𝖙𝖍𝖊 𝖂𝖔𝖗𝖑𝖉"</h1><br>
+        <h1>"𝐀  𝐋𝐈𝐓𝐓𝐋𝐄  𝐏𝐑𝐎𝐆𝐑𝐄𝐒𝐒  𝐄𝐀𝐂𝐇  𝐃𝐀𝐘   𝐀𝐃𝐃  𝐔𝐏𝐓𝐎   𝐓𝐇𝐄  𝐁𝐈𝐆 𝐑𝐄𝐒𝐔𝐋𝐓𝐒"</h1>
+    </center>
+    <br><br><br>
+    <a href="https://www.iitm.ac.in/"><img border="0" alt="iit.jpg" src="iit.jpg" width="200" height="100"></a>
+    <a href="https://www.annauniv.edu/"><img border="0" alt="annauniversity.jpg" src="annauniversity.jpg" width="200" height="100"></a>
+    <a href="https://www.tcarts.in/"><img border="0" alt="thiyagarajar.jpg" src="thiyagarajar.jpg" width="200" height="100"></a>
+    <a href="https://skcet.ac.in/"><img border="0" alt="large_Sri_Krishna_College_of_Technology_Coimbatore_1d4d014230_de37ad1f43.png" src="large_Sri_Krishna_College_of_Technology_Coimbatore_1d4d014230_de37ad1f43.png" width="200" height="100"></a> 
+   <a href="https://stjosephs.ac.in/"><img border="0" alt="st.jpg" src="st.jpg" width="200" height="100"></a>
+   <a href="https://panimalar.ac.in/"><img border="0" alt="panimalar.jpg" src="panimalar.jpg" width="200" height="100"></a>
+   <a href="https://drmcet.ac.in/"><img border="0" alt="mahalingam.jpg" src="mahalingam.jpg" width="200" height="100"></a>
+   <a href="https://www.kpriet.ac.in/"><img border="0" alt="kpr.jpg" src="kpr.jpg" width="200" height="100"></a>
+   <a href="https://www.amrita.edu/"><img border="0" alt="amritha.jpg" src="amritha.jpg" width="200" height="100"></a>
+   <a href="https://www.bitsathy.ac.in/"><img border="0" alt="bannari.jpg" src="bannari.jpg" width="200" height="100"></a>
+   <a href="https://vit.ac.in/"><img border="0" alt="vit.jpg" src="vit.jpg" width="200" height="100"></a>
+   <a href="https://www.saveetha.ac.in/"><img border="0" alt="saveetha.jpg" src="saveetha.jpg" width="200" height="100"></a>
+   <a href="https://accet.ac.in/"><img border="0" alt="alagappa.jpg" src="alagappa.jpg" width="200" height="100"></a>
+   <a href="https://www.kongu.ac.in/"><img border="0" alt="unnamed.jpg" src="unnamed.jpg" width="200" height="100"></a>
+    <br><br><br><br>
+    <div class="container mt-3">
+        <a href="https://engineering.careers360.com/"><button type="button" class="btn btn-outline-secondary">MORE COLLEGES</button></a>
+        <a href="search.php"><button type="button" class="btn btn-outline-secondary">SEARCH</button></a>
+        <a href="about.php"><button type="button" class="btn btn-outline-secondary">ABOUT</button></a>
+        <a href="https://engineering.careers360.com/"><button type="button" class="btn btn-outline-secondary">GRADE</button></a>
+        <a href="contact.php"><button type="button" class="btn btn-outline-danger">CONTACT</button>
+        <a href="https://www.shiksha.com/"><button type="button" class="btn btn-outline-secondary">RANKING</button></a>
+    </div>
+    <br><br>
+    <!-- Carousel -->
+    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+      <!-- Indicators/dots -->
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+      </div>
+      <!-- The slideshow/carousel -->
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <center><img src="colleges.jpg" alt="college image" class="d-block" style="width: 30%"></center>
+        </div>
+        <div class="carousel-item">
+            <center><img src="bg6.jpg" alt="AI image" class="d-block" style="width: 30%"></center>
+          </div>
+          <div class="carousel-item">
+            <center><img src="carousel.jpg" alt="coding image" class="d-block" style="width: 50%"></center>
+          </div>
+      </div>
+      <!-- Left and right controls/icons -->
+      <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </button>
+    </div>
+    <footer>
+      <p>CONTACT</p>
+      <p>waytocareer@gmail.com</p>
+      <p>cell:9807563421</p><br>
+    </footer>
+</body>
+</html>
