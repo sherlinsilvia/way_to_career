701926224eb88e308caf35ac61040675330afe92
sherlinsilvia <155231823+sherlinsilvia@users.noreply.github.com>
2024-09-09 21:30:56 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Document</title>
+<style>
+    body{
+    background-image: url('bg7.jpg');
+    background-size: cover;
+    
+    
+    
+    height: 100vh;
+    padding:0;
+    margin:0;
+}
+    ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color:black;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color:white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color:grey;
+}
+div.formstyle{
+margin: 8px;
+border: 1px solid grey;
+padding-inline: 50px;
+box-shadow: 0 2px 10px black;
+padding: 20px;;
+}
+h1{
+    color:white;
+}
+h3{
+    color:white;
+}
+p{
+    color:white;
+}
+h4{
+    color:white;
+}
+</style>
+<body>
+<img src="bg13.jpg.png" class="img-circle" alt="logo image" width="200" height="136"> 
+    <ul>
+      <li><a href="home.php">Home</a></li>
+      <li><a href="signin.php">sign Up</a></li>
+      <li><a href="login.php">Login</a></li>
+  </ul><br><br>
+  <h1>CONTACT US</h1>
+  <h3>phone:9876543210</h3>
+  <h3>email:firststep@gmail.com</h3>
+  
