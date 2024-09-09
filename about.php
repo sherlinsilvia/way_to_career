3dbb6261e9e6bfebd28e8645835cee753db75618
sherlinsilvia <155231823+sherlinsilvia@users.noreply.github.com>
2024-09-09 21:29:34 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Document</title>
+<style>
+    body{
+    background-image: url('bg5.jpg');
+    background-size: cover;
+    
+    
+    
+    height: 100vh;
+    padding:0;
+    margin:0;
+}
+    ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color:black;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color:white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color:grey;
+}
+div.formstyle{
+margin: 8px;
+border: 1px solid grey;
+padding-inline: 50px;
+box-shadow: 0 2px 10px black;
+padding: 20px;;
+}
+h1{
+    color:white;
+}
+h3{
+    color:white;
+}
+p{
+    color:white;
+}
+h4{
+    color:white;
+}
+</style>
+<body>
+<img src="bg13.jpg.png" class="img-circle" alt="logo image" width="200" height="136"> 
+    <ul>
+      <li><a href="home.php">Home</a></li>
+      <li><a href="signin.php">sign Up</a></li>
+      <li><a href="login.php">Login</a></li>
+  </ul><br><br>
+<h1>HISTORY OF ENGINEERING</h1>
+<p>Engineering has existed since ancient times, when humans devised inventions such as the wedge, lever, wheel and pulley, etc.
+The term engineering is derived from the word engineer, which itself dates back to the 14th century when an engine'er 
+(literally, one who builds or operates a siege engine) referred to "a constructor of military engines".[7] In this context,
+ now obsolete, an "engine" referred to a military machine, i.e., a mechanical contraption used in war 
+ (for example, a catapult). Notable examples of the obsolete usage which have survived to the present 
+ day are military engineering corps, e.g., the U.S. Army Corps of Engineers.
+The word "engine" itself is of even older origin, ultimately deriving from the Latin ingenium (c.1250), 
+meaning "innate quality, especially mental power, hence a clever invention."[8]
+Later, as the design of civilian structures, such as bridges and buildings,
+ matured as a technical discipline, the term civil engineering[6] entered the lexicon as a way
+to distinguish between those specializing in the construction of such non-military projects and those involved in the
+discipline of military engineering.</p>
+<h1>MAIN BRANCHES</h1>
+<h3>CHEMICAL ENGINEERING</h3>
+<p>Chemical engineering is the application of physics, chemistry, biology, and engineering principles in order to carry out 
+chemical processes on a commercial scale, such as the manufacture of commodity chemicals, specialty chemicals, 
+petroleum refining, microfabrication, fermentation, and biomolecule production.
+<h3>Civil engineering</h3>
+<p>Civil engineering is the design and construction of public and private works, 
+    such as infrastructure (airports, roads, railways, water supply, and treatment etc.),
+     bridges, tunnels, dams, and buildings.[59][60] Civil engineering is traditionally broken into a
+      number of sub-disciplines, including structural engineering, environmental engineering, and surveying.
+       It is traditionally considered to be separate from military engineering.[61]</p>
+<h4>Many branches of engineering are developed nowadays like AI and other departments..</h4>
+</body>
+</html>
